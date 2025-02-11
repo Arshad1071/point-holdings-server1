5acef97c23c37512bd768c7f3d923d28afd26cea
Arshad vp <Arshad1071@yahoo.in>
2025-02-11 21:30:07 +0530

    stable1

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+// const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+// const authRoute = require("./Routes/AuthRoute");
+// const employeeRoute = require("./Routes/EmployeeRoute");
+const { MONGO_URL, PORT, REACT_APP_API_URL } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// mongoose
+//   .connect(MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("MongoDB is  connected successfully"))
+//   .catch((err) => console.error(err));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    origin: ["https://mernserver-084b166fbf29.herokuapp.com"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+// app.use("/", authRoute, employeeRoute);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
